f3f606298efe104e4399f40b1facb338135f598c
Kuan-Wei-Kuo <k.kaunwei@gmail.com>
2020-05-09 10:28:34 +0800

    Init project

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+export default function Header() {
+    const navs = process.env.blog.navs;
+    let navItems = [];
+    for(let i = 0 ; i < navs.length ; i++) {
+        const nav = navs[i];
+        navItems.push(<Nav.Link key={ 'nav-' + i } href={ nav.hred }>{ nav.name }</Nav.Link>);
+    }
+    return (
+        <Navbar bg="dark" variant="dark" className="mb-4">
+            <Container>
+                <Navbar.Brand href="/">{ process.env.blog.name }</Navbar.Brand>
+                <Nav className="justify-content-end">
+                    { navItems }
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+        blog: {
+            name: '安迪的部落格',
+            navs: [{
+                name: '首頁',
+                href: '/'
+            }, {
+                name: '歸檔',
+                href: '/archives'
+            }, {
+                name: '類別',
+                href: '/categories'
+            }]
+        }
+    }
+}
